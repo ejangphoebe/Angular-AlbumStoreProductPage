b3e6fc51b91ce4a06d2f4af8679661481eff0120
HeatherPhibi <heatherphibi@yahoo.com>
2019-11-09 16:38:31 +0300

    albumInfo changes

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescripionComponent implements OnInit {
 
-  constructor() { }
+  
+@Component({
+  selector: 'app-product-descripion',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
+})
+export class ProductDescripionComponent implements OnInit {
+  
+  albumInfo;
+
+  constructor() {
+     _productService:
+     }
 
   ngOnInit() {
+     this.productService.getAlbum(1) = subscribe( 
+     response => this.AlbumInfo = response
+     )
   }
 
+}
 }
