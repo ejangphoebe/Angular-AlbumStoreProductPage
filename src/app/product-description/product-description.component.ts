e46a386e3ee3bc8aa7feaeaa4b469cfc7b730469
ejang phoebe <heatherphibi@yahoo.com>
2019-11-09 15:15:45 +0300

    first phase changes

---
+++ src/app/product-description/product-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-descripion',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
+})
+export class ProductDescripionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
